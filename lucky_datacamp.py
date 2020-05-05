02ae90994c57eb2e5a4fc641b8c4413eb5a4e604
Sim Shang En <12458@users.noreply.github.com>
2020-05-06 00:21:08 +0800

    Added Shuffle

--- lucky_datacamp.py
+++ lucky_datacamp.py
@@ -1,5 +1,4 @@
 import csv
-import time
 import secrets
 import pprint
 rng = secrets.SystemRandom()  # Same interface as random module
@@ -14,21 +13,20 @@ csv_path = r"lucky_draw2.csv"
 
 def read(csv_path):
     """NON WEIGHTED Convert CSV to lists names, emails, schools and return lists"""
-    names, schools, emails = [], [], []
+    participants = []
     with open(csv_path, 'r') as file:
         lines = csv.reader(file)
         next(lines)  # This skips the first row of the CSV file
         for row in lines:  # row: list
-            names.append(row[1])
-            emails.append(row[2])
-            schools.append(row[3])
-    num_participants = len(names)
-    return (names, emails, schools, num_participants)
+            # includes [name, school, email]
+            participants.append([row[1], row[2], row[3]])
+    num_participants = len(participants)
+    return (participants, num_participants)
 
 
 def read_weighted(csv_path):
     """WEIGHTED Convert CSV to lists names, emails, schools and return lists"""
-    names, schools, emails = [], [], []
+    participants = []
     with open(csv_path, 'r') as file:
         lines = csv.reader(file)
         next(lines)  # This skips the first row of the CSV file
@@ -38,15 +36,15 @@ def read_weighted(csv_path):
             except:
                 chance = 1  # Default chance is 1
             for _ in range(chance):
-                names.append(row[1])
-                emails.append(row[2])
-                schools.append(row[3])
-    num_participants = len(names)
-    return (names, emails, schools, num_participants)
+                # includes [name, school, email]
+                participants.append([row[1], row[2], row[3]])
+    num_participants = len(participants)
+    return (participants, num_participants)
 
 
-def winner(names, winner_num):
+def winner(participants, winner_num):
     """Choose Winner"""
+    rng.shuffle(participants)
     winner_list = []
     used_email = set()  # Set
 
@@ -54,15 +52,12 @@ def winner(names, winner_num):
     if num_participants > winner_num:
         while len(winner_list) != winner_num:
             index = rng.randint(0, num_participants - 1)
-            if not (emails[index] in used_email):  # prevent repeats
-                winner_list.append(
-                    [names[index], emails[index], schools[index]])
-                used_email.add(emails[index])
-    # participants < prizes
-    else:
-        for index in range(0, num_participants):
-            winner_list += [[names[index], emails[index], schools[index]]]
-        rng.shuffle(winner_list)  # changes the order of names -> random
+            if not (participants[index][1] in used_email):  # prevent repeats
+                winner_list.append(participants[index])
+                used_email.add(participants[index][1])
+
+    else:  # participants < prizes
+        winner_list = participants
     return winner_list
 
 
@@ -86,7 +81,7 @@ while True:
         [1]: Draw winner
         [2]: Draw winner with weighted chances
         [3]: Set CSV file path
-        [4]: Read and print weighted current candidates
+        [4]: Read and print weighted current candidates (not random)
         [5]: Exit
         """
         )
@@ -98,21 +93,22 @@ while True:
         continue
     if menu_option == 1:
         print("Drawing Winner")
-        (names, emails, schools, num_participants) = read(
+        participants, num_participants = read(
             csv_path)  # reads file and unpack tuple
-        winner_list = winner(names, winner_num)  # returns winner list
+        winner_list = winner(participants, winner_num)  # returns winner list
         display(winner_list, prizes)  # outputs
     elif menu_option == 2:
         print("Drawing Winner with Weighted chances")
-        (names, emails, schools, num_participants) = read_weighted(
+        participants, num_participants = read_weighted(
             csv_path)  # reads file and unpack tuple
-        winner_list = winner(names, winner_num)  # returns winner list
+        winner_list = winner(participants, winner_num)  # returns winner list
         display(winner_list, prizes)  # outputs
     elif menu_option == 3:
         csv_path = input("Enter new CSV path: ")
     elif menu_option == 4:
-        (names, emails, schools, num_participants) = read_weighted(
+        participants, num_participants = read_weighted(
             csv_path)  # reads file and unpack tuple
+        names = [element[0] for element in participants]
         pp.pprint(names)
     else:
         print("Exiting...")
